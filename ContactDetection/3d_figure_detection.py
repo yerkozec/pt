4fffc76144df1199f9f5fe4bd2ca757b33566724
yerkozec <y.zecsoto@uandresbello.edu>
2019-10-01 02:02:31 -0300

    Figure in 3d with point recognition of been inside or not

---
+++ ContactDetection/3d_figure_detection.py
@@ -0,0 +1,221 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+
+# -----------------generador de figura geometricas 2d------------
+
+def genfig():
+    vectorx = random.sample(range(30), 2)
+    vectory = random.sample(range(30), 2)
+    vectorz = random.sample(range(30), 2)
+    return (vectorx, vectory, vectorz)
+
+
+def genpunto():
+    punto = random.sample(range(30), 3)
+    # punto = np.array([5, 13])
+    return punto
+
+def genarista():
+    '''vectorx = random.sample(range(30), 2)
+    vectory = random.sample(range(30), 2)'''
+    vectorx = np.array([1, 30])
+    vectory = np.array([1, 30])
+    return vectorx, vectory
+
+
+
+def getvertex(figura):
+    vertex0 = np.array([figura[0][0],
+                        figura[1][0],
+                        figura[2][0]])
+
+    vertex1 = np.array([figura[0][1],
+                        figura[1][0],
+                        figura[2][0]])
+
+    vertex2 = np.array([figura[0][0],
+                        figura[1][1],
+                        figura[2][0]])
+
+    vertex3 = np.array([figura[0][0],
+                        figura[1][0],
+                        figura[2][1]])
+    return (vertex0, vertex1, vertex2, vertex3)
+
+
+def getB(figura):
+    vertex = np.array(getvertex(figura))
+    U0 = np.array(vertex[0])
+    U1 = np.array(vertex[1])
+    U2 = np.array(vertex[2])
+    U3 = np.array(vertex[3])
+    # ----------------------------------------
+    b1 = U1 - U0
+    b2 = U2 - U0
+    b3 = U3 - U0
+    print("\nb1:\n", b1, "\nb2:\n", b2, "\nb3:\n",b3)
+
+    normb1 = np.linalg.norm(b1, ord=2)
+    normb2 = np.linalg.norm(b2, ord=2)
+    normb3 = np.linalg.norm(b3, ord=2)
+
+    print("\nnorma1:\n", normb1, "\nnorma2:\n", normb2, "\nnorma3:\n", normb3)
+
+    b1v = (b1 / normb1)
+    b2v = (b2 / normb2)
+    b3v = (b3 / normb3)
+
+    B = np.array([[b1v[0], b1v[1], b1v[2]],
+                  [b2v[0], b2v[1], b2v[2]],
+                  [b3v[0], b3v[1], b3v[2]]])
+    return B
+
+
+def pointdetection(figura, punto):
+    vertex = np.array(getvertex(figura))
+    U0 = np.array(vertex[0])
+    U1 = np.array(vertex[1])
+    U2 = np.array(vertex[2])
+    U3 = np.array(vertex[3])
+
+    # ----------------------------------------
+    a = U1 - U0
+    c = U2 - U0
+    b = U3 - U0
+
+    B = getB(figura)
+
+    V0 = punto[0] - U0[0]
+    V1 = punto[1] - U0[1]
+    V2 = punto[2] - U0[2]
+
+    V = np.array([[V0], [V1], [V2]])
+
+    alpha = np.linalg.solve(B, V)
+    print("\n Alfa: \n", alpha)
+
+    resultx, resulty, resultz = False, False, False
+
+    if (0 <= alpha[0] <= max(a)):
+        resultx = True
+    if (0 <= alpha[1] <= max(c)):
+        resulty = True
+    if (0 <= alpha[2] <= max(b)):
+        resultz = True
+    return resultx and resulty and resultz
+
+
+'''
+def aristadetection(figura, arista):
+    # ----------------------- get vectores de las figuras------------
+    vertex = np.array(getvertex(figura))
+    U0 = np.array(vertex[0])
+    U1 = np.array(vertex[1])
+    U2 = np.array(vertex[2])
+    U3 = np.array(vertex[3])
+    # -----------------------get vectores de las aristas--------------
+    Vj = np.array([arista[0][0], arista[1][0]])
+    Vk = np.array([arista[0][1], arista[1][1]])
+    s = np.random.sample(1)
+    rS = ((1 - s) * Vj) + s * Vk
+    print(" R*(s): ", rS)
+    B = getB(figura)
+    ro = np.linalg.solve(B, rS)
+    print(" ro: ", ro)
+    a = U1 - U0
+    b = U3 - U0
+
+    resultx, resulty = False, False
+    if (0 <= ro[0] <= max(a)):
+        resultx = True
+    if (0 <= ro[1] <= max(b)):
+        resulty = True
+    return resultx and resulty
+    '''
+
+'''
+aristainside = aristadetection(figura, TestArista)
+inside = pointdetection(figura, pts)
+'''
+
+# ----------Define plano cartesiano de trabajo----------------
+
+punto = genpunto()
+figura = genfig()
+
+figura[0].sort()
+figura[1].sort()
+figura[2].sort()
+
+xline = np.array(figura[0])
+yline = np.array(figura[1])
+zline = np.array(figura[2])
+
+print("axis x:\n", xline, "\n y: \n", yline, "\n z: \n", zline)
+print("punto: \n", punto)
+inside = pointdetection(figura, punto)
+print(inside)
+
+# ----------------------------Config Plano catersiano----------------------
+ax = plt.axes(projection = '3d')
+ax.set_xlim3d(0, 30)
+ax.set_ylim3d(0, 30)
+ax.set_zlim3d(0, 30)
+ax.set_xlabel('X axis')
+ax.set_ylabel('Y axis')
+ax.set_zlabel('Z axis')
+
+# ----------------------------Dibujo 3d--------------------------
+ax.plot3D((xline[0], xline[1], xline[1], xline[0], xline[0]),
+          (yline[0], yline[0], yline[1], yline[1], yline[0]),
+          zline[0],
+          color= 'm')
+ax.plot3D((xline[0], xline[1], xline[1], xline[0], xline[0]),
+          (yline[0], yline[0], yline[1], yline[1], yline[0]),
+          zline[1],
+          color = 'c')
+ax.plot3D((xline[0], xline[0]),
+          (yline[0], yline[0]),
+          (zline[0], zline[1]),
+          color = 'r')
+ax.plot3D((xline[1], xline[1]),
+          (yline[0], yline[0]),
+          (zline[0], zline[1]),
+          color = 'r')
+ax.plot3D((xline[1], xline[1]),
+          (yline[1], yline[1]),
+          (zline[0], zline[1]),
+          color = 'r')
+ax.plot3D((xline[0], xline[0]),
+          (yline[1], yline[1]),
+          (zline[0], zline[1]),
+          color = 'r')
+
+# ----------------------------- Dibuja un punto si esta dentro de un color y si esta fuera de otro -----------------------
+if(inside):
+    ax.plot3D((punto[0], punto[0]),
+              (punto[1], punto[1]),
+              (punto[2], punto[2]),
+              'o',
+              color = 'g')
+else:
+    ax.plot3D((punto[0], punto[0]),
+              (punto[1], punto[1]),
+              (punto[2], punto[2]),
+              'o',
+              color='r')
+
+'''
+# ---------------------------------Dibuja arista de prueba-------------------------------
+if (aristainside):
+    ax.plot(TestArista[0], TestArista[1], 'o-', color='c')
+else:
+    ax.plot(TestArista[0], TestArista[1], 'o-', color='r')
+
+'''
+
+plt.grid()
+plt.show()
--- ContactDetection/Generador_figuras.py
+++ ContactDetection/Generador_figuras.py
@@ -2,71 +2,127 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
-#-----------------generador de figura geometricas 2d------------
+
+# -----------------generador de figura geometricas 2d------------
 
 def genfig():
-    
+    '''
     vectorx = random.sample(range(30), 2)
     vectory = random.sample(range(30), 2)
     '''
-    vectorx = np.array([5, 20])
-    vectory = np.array([7, 13])
-'''
+    vectorx = np.array([3, 25])
+    vectory = np.array([3, 25])
+
     return (vectorx, vectory)
 
+
 def genpunto():
-    punto = random.sample(range(20), 2)
-    #punto = np.array([5, 13])
+    punto = random.sample(range(30), 2)
+    # punto = np.array([5, 13])
     return punto
 
-def pointdetection(figura,punto):
-    vertex = np.array(getvertex(figura))
-    U0 = np.array(vertex[0])
-    U1 = np.array(vertex[1])
-    U2 = np.array(vertex[2])
-    U3 = np.array(vertex[3])
-    #----------------------------------------
-    b1 = U1 - U0
-    b3 = U3 - U0
-    print("\n b1: \n", b1, "\n b3:\n", b3)
 
-    normb1 = np.linalg.norm(b1, ord=2)
-    normb3 = np.linalg.norm(b3, ord=2)
+def genarista():
+    '''vectorx = random.sample(range(30), 2)
+    vectory = random.sample(range(30), 2)'''
+    vectorx = np.array([5, 15])
+    vectory = np.array([5, 25])
+    return vectorx, vectory
 
-    print("\n norma1: \n", normb1, "\n norma0: \n", normb3)
-
-    b1v = (b1/ normb1)
-    b3v = (b3/ normb3)
-
-    B = np.array([[b1v[0], b1v[1]],
-                [b3v[0], b3v[1]]])
-
-    print("\n B: \n", B)
-    V0 = punto[0]-U0[0]
-    V1 = punto[1]-U0[1]
-
-    V = np.array([[V0], [V1]])
-    print("\n V: \n", V)
-    alpha = np.linalg.solve(B, V)
-    print("\n Alfa: \n", alpha)
-    resultx,resulty = False, False
-    if(0 <= alpha[0] <= max(b1)):
-        resultx = True
-    if(0 <= alpha[1] <= max(b3)):
-        resulty = True
-    return resultx and resulty
-
-
-pts = genpunto()
-figura = np.array(genfig())
-print("figura (eje x , eje y): ", figura)
 
 def getvertex(figura):
     vertex0 = np.array([min(figura[0]), min(figura[1])])
     vertex1 = np.array([max(figura[0]), min(figura[1])])
     vertex2 = np.array([max(figura[0]), max(figura[1])])
     vertex3 = np.array([min(figura[0]), max(figura[1])])
-    return (vertex0,vertex1,vertex2,vertex3)
+    return (vertex0, vertex1, vertex2, vertex3)
+
+
+def getB(figura):
+    vertex = np.array(getvertex(figura))
+    U0 = np.array(vertex[0])
+    U1 = np.array(vertex[1])
+    U2 = np.array(vertex[2])
+    U3 = np.array(vertex[3])
+    # ----------------------------------------
+    b1 = U1 - U0
+    b3 = U3 - U0
+
+    normb1 = np.linalg.norm(b1, ord=2)
+    normb3 = np.linalg.norm(b3, ord=2)
+
+    b1v = (b1 / normb1)
+    b3v = (b3 / normb3)
+
+    B = np.array([[b1v[0], b1v[1]],
+                  [b3v[0], b3v[1]]])
+    return B
+
+
+def pointdetection(figura, punto):
+    vertex = np.array(getvertex(figura))
+    U0 = np.array(vertex[0])
+    U1 = np.array(vertex[1])
+    U2 = np.array(vertex[2])
+    U3 = np.array(vertex[3])
+    # ----------------------------------------
+    a = U1 - U0
+    b = U3 - U0
+
+    B = getB(figura)
+
+    V0 = punto[0] - U0[0]
+    V1 = punto[1] - U0[1]
+
+    V = np.array([[V0], [V1]])
+
+    alpha = np.linalg.solve(B, V)
+
+    resultx, resulty = False, False
+    if (0 <= alpha[0] <= max(a)):
+        resultx = True
+    if (0 <= alpha[1] <= max(b)):
+        resulty = True
+    return resultx and resulty
+
+
+def aristadetection(figura, arista):
+    # ----------------------- get vectores de las figuras------------
+    vertex = np.array(getvertex(figura))
+    U0 = np.array(vertex[0])
+    U1 = np.array(vertex[1])
+    U2 = np.array(vertex[2])
+    U3 = np.array(vertex[3])
+    # -----------------------get vectores de las aristas--------------
+    Vj = np.array([arista[0][0], arista[1][0]])
+    Vk = np.array([arista[0][1], arista[1][1]])
+    s = np.random.sample(1)
+
+    rS = ((1 - s) * Vj) + s * Vk
+
+    print(" R*(s): ", rS)
+
+    B = getB(figura)
+
+    ro = np.linalg.solve(B, rS)
+    print(" ro: ", ro)
+
+    a = U1 - U0
+    b = U3 - U0
+
+    resultx, resulty = False, False
+    if (0 <= ro[0] <= max(a)):
+        resultx = True
+    if (0 <= ro[1] <= max(b)):
+        resulty = True
+    return resultx and resulty
+
+
+pts = genpunto()
+figura = np.array(genfig())
+TestArista = np.array(genarista())
+print("figura (eje x , eje y): ", figura)
+print("arista test: ", TestArista)
 
 vertex = np.array(getvertex(figura))
 vertex0 = vertex[0]
@@ -77,22 +133,44 @@ vertex3 = vertex[3]
 init = np.array(vertex0)
 fin = np.array(vertex3)
 
-print("punto: ", pts)
-print("vertice0: ", vertex0)
-print("vertice1: ", vertex1)
-print("vertice2: ", vertex2)
-print("vertice3: ", vertex3)
+
+aristainside = aristadetection(figura, TestArista)
 inside = pointdetection(figura, pts)
 
-#Generar rectangulo
+# Generar rectangulo
+# ----------Config plano cartesiano de trabajo----------------
+
 fig, ax = plt.subplots(figsize=(10, 5))
-plt.axis([0,30,0,30])
-ax.plot((vertex0[0], vertex1[0], vertex2[0], vertex3[0]),(vertex0[1], vertex1[1], vertex2[1], vertex3[1]), 'o-', color='g')
-ax.plot((init[0], fin[0]), (init[1], fin[1]), 'o-', color='b')
-if(inside):
-    ax.plot(pts[0], pts[1], 'o', color='m')
+plt.axis([0, 30, 0, 30])
+
+# ------------------------------------- Dibuja la figura -----------------------------------
+ax.plot((vertex0[0], vertex1[0], vertex2[0], vertex3[0], vertex0[0]),
+        (vertex0[1], vertex1[1], vertex2[1], vertex3[1], vertex0[1]),
+        'o-',
+        color='g')
+
+# ----------------------------- Dibuja un punto si esta dentro de un color y si esta fuera de otro -----------------------
+if (inside):
+    ax.plot(pts[0],
+            pts[1],
+            'o',
+            color='m')
 else:
-    ax.plot(pts[0], pts[1], 'o', color='r')
-plt.fill_between((vertex2[0], vertex3[0]), (vertex2[1]), vertex1[1])
+    ax.plot(pts[0],
+            pts[1],
+            'o',
+            color='r')
+# ---------------------------------Dibuja arista de prueba-------------------------------
+if (aristainside):
+    ax.plot(TestArista[0],
+            TestArista[1],
+            'o-',
+            color='c')
+else:
+    ax.plot(TestArista[0],
+            TestArista[1],
+            'o-',
+            color='r')
+
 plt.grid()
 plt.show()
